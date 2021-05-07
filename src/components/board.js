c070a081da08d97861ebf9bf37932f31e419bc82
David Johnson <dcj12371@msn.com>
2021-05-07 11:30:02 -0500

    starting board component

---
+++ src/components/board.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export class Board extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h2>Game Board</h2>
+      </div>
+    )
+  }
+}
+
+export default Board
