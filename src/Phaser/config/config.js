5e9bf2f266cf9354aed3a2bcde5d6c7ba3e7ae6e
Joe Alves <joseph.michael.alves@gmail.com>
2021-05-10 15:56:57 -0400

    ref example

--- src/Phaser/config/config.js
+++ src/Phaser/config/config.js
@@ -2,7 +2,6 @@ import Phaser from 'phaser';
 
 export default {
   type: Phaser.AUTO,
-  parent: 'game-canvas',
   width: 800,
   height: 600,
   physics: {
--- src/components/GameCanvas.js
+++ src/components/GameCanvas.js
@@ -5,17 +5,23 @@ import config from '../Phaser/config/config';
 export default class GameCanvas extends React.Component {
   constructor() {
     super();
+    this.canvasRef = React.createRef();
   }
 
   componentDidMount() {
-    new Game(config);
+    console.log(this.canvasRef.current);
+    new Game({...config, canvas: this.canvasRef.current, parent: null });
   }
 
   shouldComponentUpdate() {
     return false;
   }
 
+  componentDidUpdate() {
+    console.log(this.canvasRef.current);
+  }
+
   render() {
-    return <div id="game-canvas"></div>;
+    return <canvas ref={this.canvasRef} id="game-canvas"></canvas>;
   }
 }
