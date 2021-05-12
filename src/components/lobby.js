cd52bf30e3d4f6b07d98a12e4d5bf653147673a7
David Johnson <dcj12371@msn.com>
2021-05-12 14:52:23 -0400

    converted js to react component

--- src/components/lobby.js
+++ src/components/lobby.js
@@ -9,14 +9,17 @@ export class Lobby extends React.Component {
       gameReady: false,
       gameId: null,
       sprites: [
-        { name: "Kratos", imgUrl: '/pubic/kratos-avatar-1.jpg'},
-        { name: "Donkey Kong", imgUrl: '/pubic/donkey-kong-avatar-2.jpg'},
-        { name: "Link", imgUrl: '/pubic/link-avatar-1.jpg'},
-        { name: "Sonic", imgUrl: '/pubic/sonic-avatar-2.jpg'},
-      ]
+        { name: "Kratos", imgUrl: 'kratos-avatar-1.jpg'},
+        { name: "Donkey Kong", imgUrl: 'donkey-kong-avatar-2.jpg'},
+        { name: "Link", imgUrl: 'link-avatar-1.jpg'},
+        { name: "Sonic", imgUrl: 'sonic-avatar-2.jpg'},
+      ],
+      avatarThumb: 'kratos-avatar-1.jpg',
+      avatarName: "Kratos",
+      selectedColor: 'black'
     };
-    this.handleColorSelect.bind = this.handleColorSelect;
-    this.handleAvatarNav.bind = this.handleAvatarNav;
+    this.handleColorSelect = this.handleColorSelect.bind(this);
+    this.handleAvatarNav = this.handleAvatarNav.bind(this);
   }
 
   componentDidMount() {
@@ -33,68 +36,77 @@ export class Lobby extends React.Component {
 
   handleColorSelect(evt) {
     const color = evt.target.value === ''? 'Black' : evt.target.value;
-    const box = document.getElementById('selected-color');
-    box.style.backgroundColor = `${color}`;
+    this.setState(
+      {
+        selectedColor: color
+      }
+    )
   }
 
   handleAvatarNav(evt) {
-    currAvatar = document.getElementById('avatar-name').value;
-    let elementPos = sprites.map(function(x) {return x.name; }).indexOf(currAvatar);
-    if(elementPos === 0 || elementPos === 3) { //at either boundary, do nothing
-      return
+    const currAvatar = this.state.avatarName;
+    const direction = Number(evt.target.value);
+    let elementPos = this.state.sprites.map(function(x) { return x.name; }).indexOf(currAvatar);
+
+    if((elementPos === 0 && direction === 1) || (elementPos === 3 && direction === -1))
+    {
+      return //at either boundary, do nothing
     } else {
-      const direction = evt.target.value;
-      const avatarFound = sprites[elementPos - direction];
-      const img = document.getElementById('avatar-thumb');
-      img.src = avatarFound.imgUrl;
-      const textbox = document.getElementById('avatar-name');
-      textbox.value = avatarFound.name;
+      const avatarFound = this.state.sprites[elementPos - direction];
+      this.setState(
+        {
+          avatarThumb: avatarFound.imgUrl,
+          avatarName: avatarFound.name
+        }
+      )
     }
   }
 
 
   render() {
 
+    const { avatarThumb, avatarName, selectedColor } = this.state;
+
     return (
       <div id="lobby-window">
         <h2 className="title">FS Party</h2>
           <form name={name} id="lobby-form">
-        <div className="form-row">
+          <div className="form-row">
             <div className="form-col">
             <div className="input-div">
               <label htmlFor="playername">
-                <small>Enter Name</small>
+                Enter Name
               </label>
               <input name="playername" type="text" className="lobby-input" />
             </div>
             <div className="input-div">
               <label htmlFor="playercolor">
-                <small>Select Color</small>
+                Select Color
               </label>
-              <select name="playercolor" className="lobby-input" onchange={this.handleColorSelect}>
+              <select name="playercolor" className="lobby-input" onChange={this.handleColorSelect}>
               <option value=""></option>
-              <option value="Blue">Blue</option>
-              <option value="Red">Red</option>
-              <option value="Green">Green</option>
-              <option value="Yellow">Yellow</option>
+              <option value="blue">Blue</option>
+              <option value="red">Red</option>
+              <option value="green">Green</option>
+              <option value="yellow">Yellow</option>
               </select>
-              <div id="selected-color" className="selected-color"></div>
+              <div id="selected-color" className={`selected-color ${selectedColor}`}></div>
             </div>
           </div>
           <div className="form-col">
             <div className="input-div">
               <div name="sprite-image"></div>
               <label htmlFor="playersprite">
-              <small>Select Character</small>
+                Select Character
               </label>
               <div className="form-row">
-                <button type="button" id="prev-sprite" onclick={this.handleAvatarNav} value="1">prev</button>
-                <img id="avatar-thumb" src="kratos-avatar-1.jpg" className="avatar"/>
-                <button type="button" id="next-sprite" onclick={this.handleAvatarNav} value="-1">next</button>
+                <button type="button" id="prev-sprite" onClick={this.handleAvatarNav} value="1">prev</button>
+                <img id="avatar-thumb" src={avatarThumb} className="avatar"/>
+                <button type="button" id="next-sprite" onClick={this.handleAvatarNav} value="-1">next</button>
               </div>
             </div>
             <div id="sel-color-div" className="input-div">
-              <input id="avatar-name" name="playersprite" type="hidden" className="lobby-input" value="Kratos" />
+              <input id="avatar-name" name="playersprite" type="text" className="lobby-input" value={avatarName} />
             </div>
           </div>
           </div>
