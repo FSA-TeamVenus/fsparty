e39448b963004c514fc823ed4550a0b6d6b166ca
gg447062 <gg447062@gmail.com>
2021-05-18 12:29:40 -0500

    adds start screen

---
+++ src/Phaser/scenes/StartScreen.js
@@ -0,0 +1,33 @@
+import Phaser from 'phaser';
+
+export default class StartScreen extends Phaser.Scene {
+  constructor() {
+    super('startScreen');
+  }
+
+  init(data) {
+    this.nextScene = data.nextScene;
+    this.instructions = data.instructions;
+  }
+
+  create() {
+    this.add.text(300, 150, 'Instructions');
+    this.add.text(300, 250, `${this.instructions}`);
+    this.add.text(300, 300, `hit ENTER to start`);
+
+    this.enter = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.ENTER
+    );
+
+    this.enter.on('down', () => {
+      this.time.addEvent({
+        delay: 2000,
+        callback: () => {
+          this.scene.start(`${this.nextScene}`);
+        },
+        callbackScope: this,
+        loop: false,
+      });
+    });
+  }
+}
