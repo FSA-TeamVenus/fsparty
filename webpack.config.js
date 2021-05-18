552eaa6c247d0de749577b17121f7637f9d1d915
gg447062 <gg447062@gmail.com>
2021-05-18 15:48:25 -0500

    removed publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js',
-    publicPath: '/',
   },
   devServer: {
     contentBase: path.join(__dirname, 'docs'),
