d628a8e13e8f06b5a7b3ce602970e6138aa60c6d
gg447062 <gg447062@gmail.com>
2021-05-20 18:22:45 -0500

    adds styling

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -8,13 +8,12 @@ export default class Tile extends React.Component {
     const { id, actionType } = this.props.tileProps;
     const { players, dictionary } = this.props;
     return (
-      <div className={`tile ${actionType}`}>
+      <div className={`tile ${actionType} flex-cont-row`}>
         {dictionary
           ? players.map((player) => {
               if (dictionary[player.position].id === id) {
                 return (
                   <div key={player.playerId}>
-                    {/* {player.name[0]} */}
                     <img
                       className="tile-sprite"
                       src={player.spriteUrl}
